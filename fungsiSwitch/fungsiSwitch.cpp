6dab6d888ed60a0c2a112109c0eaaee792cab275
LENOVO <MeutyaAzzahrani>
2024-04-29 13:34:55 +0700

    Add project files.

---
+++ fungsiSwitch/fungsiSwitch.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+using namespace std;
+
+int bilSatu, bilDua;
+
+void input()
+{
+    cout << "Masukan Bilangan Pertama =";
+    cin >> bilSatu;
+    cout << "Masukan Bilangan Kedua =";
+    cin >> bilDua;
+}
+float penjumlahan(float a, float b)
+{
+    return a + b;
+}
+float pembagian(float a, float b)
+{
+    return a / b;
+}
+float perkalian(float a, float b)
+{
+    return a * b;
+}
+float pengurangan(float a, float b)
+{
+    return a - b;
+}
+
+int  main()
+{
+    int pilihan;
+
+    do
+    {
+        cout << "===========================" << endl;
+        cout << "MENU" << endl;
+        cout << "1. penjumlahan " << endl;
+        cout << "2. pembagian " << endl;
+        cout << "3. perkalian " << endl;
+        cout << "4. pengurangan " << endl;
+        cout << "5. exit" << endl;
+        cout << "masukan pilihan 1/2/3/4/5 = ";
+        cin >> pilihan;
+
+        switch (pilihan)
+        {
+        case 1:
+            input();
+            cout << "hasil penjumlahan = " << penjumlahan(bilSatu, bilDua) << endl;
+            break;
+        case 2:
+            input();
+            cout << "hasil pembagian = " << pembagian(bilSatu, bilDua) << endl;
+            break;
+        case 3:
+            input();
+            cout << "hasil perkalian = " << perkalian(bilSatu, bilDua) << endl;
+            break;
+        case 4:
+            input();
+            cout << "hasil pengurangan = " << pengurangan(bilSatu, bilDua) << endl;
+            break;
+        case 5:
+            input();
+            cout << "pilihan tidak ada= " << endl;
+            break;
+
+        }
+
+    } while (pilihan != 5);
+
+    return 0;
+
+}
